008deb103e0c0550650b0f28d0e63155e7f5b164
AvocadoMoon <casualcucumber59@gmail.com>
2023-08-03 14:32:48 -0400

    Open General N5 Files
    Can now open any N5 file that is given to the script, rather than manually inputting a string into the script.

--- src/main/java/org/vcell/vcellfiji/N5ImageHandler.java
+++ src/main/java/org/vcell/vcellfiji/N5ImageHandler.java
@@ -1,45 +1,85 @@
 package org.vcell.vcellfiji;
 
 
-import ij.ImagePlus;
 import net.imagej.ImageJ;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.cache.img.CachedCellImg;
-import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+import org.janelia.saalfeldlab.n5.metadata.N5Metadata;
+import org.janelia.saalfeldlab.n5.metadata.N5MetadataParser;
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
 import ij.IJ;
 import org.janelia.saalfeldlab.n5.*;
 import org.janelia.saalfeldlab.n5.imglib2.*;
 import net.imglib2.img.display.imagej.*;
+import org.vcell.vcellfiji.VCellGUI;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // Command plugins
 @Plugin(type = Command.class, menuPath = "Plugins>VCell>ImageHandler")
 public class N5ImageHandler implements Command{
 
+    private N5TreeNode rootNode;
+
 
     @Override
     public void run() {
-        ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
-        image.show();
+        VCellGUI vGui = new VCellGUI();
+        vGui.localFileDialog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                readN5Files(vGui.localFileDialog.getSelectedFile());
+            }
+        });
+//        readN5Files();
 
+    }
+
+
+    private void readN5Files(File n5File){
         // auto closes reader
-        try (N5FSReader n5FSReader = new N5FSReader("D:\\Downloads\\Images\\N5\\test_image.n5")) {
-            CachedCellImg<UnsignedShortType, ?> n5imp = N5Utils.open(n5FSReader, "test\\c0\\s0");
-//            System.out.print(n5FSReader.getDatasetAttributes("test"));
+        try (N5FSReader n5FSReader = new N5FSReader(n5File.getPath())) {
+            ExecutorService loaderExecuter = Executors.newCachedThreadPool();
+            ArrayList<N5MetadataParser<?>> groupParsers = new ArrayList<>();
+            ArrayList<N5MetadataParser<?>> parserList = new ArrayList<>();
+            N5TreeNode rootPath = new N5TreeNode(n5File.getPath());
+
+            N5DatasetDiscoverer datasetDiscoverer = new N5DatasetDiscoverer(n5FSReader, loaderExecuter, parserList, groupParsers);
+
+//            String[] datasetPaths = n5FSReader.deepList(n5File.getPath(), loaderExecuter);
+
+
+//            https://github.com/saalfeldlab/n5-imglib2/blob/master/src/test/java/org/janelia/saalfeldlab/n5/imglib2/N5DatasetViewerExample.java
+            N5TreeNode n5TreeNode = datasetDiscoverer.discoverAndParseRecursive("/");
+            String[] metaList = n5FSReader.deepList("/");
+            ArrayList<String> fList = new ArrayList<>();
+
+            for (String s : metaList) {
+                if (n5FSReader.datasetExists(s)) {
+                    fList.add(s);
+                };
+            }
+
+
+            CachedCellImg<UnsignedShortType, ?> n5imp = N5Utils.open(n5FSReader, fList.get(0));
             ImageJFunctions.show(n5imp);
-            System.out.print(n5FSReader.toString());
-        } catch (IOException e) {
+//            System.out.print(n5FSReader.toString());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
-        new ImageJ();
         new N5ImageHandler().run();
-
     }
 }
