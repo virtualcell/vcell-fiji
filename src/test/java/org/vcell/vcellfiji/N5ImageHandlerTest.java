8316eb05618566b718a5d557e75412a18d1bff0b
AvocadoMoon <casualcucumber59@gmail.com>
2023-08-07 11:35:44 -0400

    Changed Test Case
    Changed the current tests to be more concise and less vague about what they are testing.

--- src/test/java/org/vcell/vcellfiji/N5ImageHandlerTest.java
+++ src/test/java/org/vcell/vcellfiji/N5ImageHandlerTest.java
@@ -6,14 +6,19 @@ import junit.framework.TestCase;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class N5ImageHandlerTest extends TestCase {
 
-    public void testReadBasicN5File() throws URISyntaxException {
+    public void testN5DatasetList() throws URISyntaxException {
         N5ImageHandler n5ImageHandler = new N5ImageHandler();
         URL url = ClassLoaderUtil.getClassLoader().getResource("N5/test_image.n5");
         File n5File = new File(url.toURI().getPath());
-        n5ImageHandler.readN5Files(n5File);
+        n5ImageHandler.setSelectedFile(n5File);
+        ArrayList<String> datasetList = n5ImageHandler.getN5DatasetList();
+
+        assertEquals("test/c0/s0", datasetList.get(0));
+        assertEquals(1, datasetList.size());
     }
 
 
