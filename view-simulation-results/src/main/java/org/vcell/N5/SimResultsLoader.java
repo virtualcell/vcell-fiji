a8a04947e1e3ef0e3c47c30375513af793f30b5a
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-03-25 11:17:03 -0400

    Current State of Code
    Committing my current pom and other files so that they can be reviewed by other people

--- view-simulation-results/src/main/java/org/vcell/N5/SimResultsLoader.java
+++ view-simulation-results/src/main/java/org/vcell/N5/SimResultsLoader.java
@@ -10,12 +10,8 @@ import com.amazonaws.services.s3.AmazonS3URI;
 import ij.ImagePlus;
 import net.imglib2.cache.img.CachedCellImg;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.img.imageplus.ImagePlusImgs;
-import net.imglib2.type.numeric.integer.*;
 import net.imglib2.type.numeric.real.DoubleType;
-import net.imglib2.type.numeric.real.FloatType;
 import org.janelia.saalfeldlab.n5.N5FSReader;
-import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 import org.janelia.saalfeldlab.n5.s3.N5AmazonS3Reader;
 
@@ -24,7 +20,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -97,22 +92,12 @@ public class SimResultsLoader {
     public ArrayList<String> getS3N5DatasetList(){
 
         // used as a flag to tell that remote access is occurring, and that there is no local files
-        selectedLocalFile = null;
-
         try(N5AmazonS3Reader n5AmazonS3Reader = new N5AmazonS3Reader(this.s3Client, this.bucketName)) {
             return new ArrayList<>(Arrays.asList(n5AmazonS3Reader.deepListDatasets(this.s3ObjectKey)));
         }
     }
-    public N5AmazonS3Reader getN5AmazonS3Reader(){
-        return new N5AmazonS3Reader(this.s3Client, this.bucketName, this.s3ObjectKey);
-    }
 
-    public N5FSReader getN5FSReader(){
-        return new N5FSReader(selectedLocalFile.getPath());
-
-    }
-
-    public ArrayList<String> getN5DatasetList(){
+    public ArrayList<String> getN5DatasetList() throws IOException {
         // auto closes reader
         try (N5FSReader n5Reader = new N5FSReader(selectedLocalFile.getPath())) {
             String[] metaList = n5Reader.deepList("/");
@@ -129,22 +114,14 @@ public class SimResultsLoader {
     public void setSelectedLocalFile(File selectedLocalFile){
         this.selectedLocalFile = selectedLocalFile;
     }
-    public File getSelectedLocalFile() {
-        return selectedLocalFile;
-    }
 
     public ImagePlus getImgPlusFromN5File() throws IOException {
         return getImgPlusFromN5File(dataSetChosen);
     }
 
     public ImagePlus getImgPlusFromN5File(String selectedDataset) throws IOException {
-        N5Reader n5Reader = selectedLocalFile != null ? getN5FSReader() : getN5AmazonS3Reader();
-        // Theres definitly a better way to do this, I trie using a variable to change the cached cells first parameter type but it didn't seem to work :/
-        return ImageJFunctions.wrap((CachedCellImg<DoubleType, ?>) N5Utils.open(n5Reader, selectedDataset), selectedDataset);
-
-//                final ARGBARGBDoubleConverter<ARGBDoubleType> converters = new ARGBARGBDoubleConverter<>();
-//                final CachedCellImg<DoubleType, ?> cachedCellImg = N5Utils.open(n5Reader, selectedDataset);
-//                return ImageJFunctions.showRGB(cachedCellImg, converters, "");
+        N5AmazonS3Reader n5AmazonS3Reader = new N5AmazonS3Reader(s3Client, bucketName, s3ObjectKey);
+        return ImageJFunctions.wrap((CachedCellImg<DoubleType, ?>) N5Utils.open(n5AmazonS3Reader, selectedDataset), selectedDataset);
     }
 
     public void setURI(URI uri){
